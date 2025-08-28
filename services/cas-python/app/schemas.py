62d4b8d5eedf52a12d3283672b6fc9822ba2f283
Jonatan Alvarado <jalvaradoa14@miumg.edu.gt>
2025-08-27 23:07:41 -0600

    refactor. mover SolverRequest a schemas.py y mantener interfaz HTML en main.py

--- services/cas-python/app/schemas.py
+++ services/cas-python/app/schemas.py
@@ -1,1 +1,16 @@
 from pydantic import BaseModel
+
+# Modelo de entrada: lo que el cliente envía al servidor
+class IntegralRequest(BaseModel):
+    expression: str # Ejemplo: "x*exp(2*x)"
+    variable: str = "x" # Por defecto es "x" si no se manda otra variable
+
+# Modelo de salida: lo que la API devuelve al cliente
+class IntegralResponse(BaseModel):
+    original: str
+    result: str
+
+# Que hice aquí?
+# Usamos Pydantic (BaseModel) para validar la entrada y salida.
+# Si se envía algo que no coincide con IntegralRequest, FastAPI devuelve un error automáticamente.
+# Swagger (/docs) mostrará estos modelos y permitirá probar la API fácilmente.
